72a31590b5003298c3e9cb7ac2da63a63b3ff074
clobochar <clo.bochar_@hotmail.com>
2022-10-20 09:41:20 +0100

    Refactoring showList

--- js/main.js
+++ js/main.js
@@ -14,136 +14,34 @@ function uuidv4() {
   );
 }
 
-const studentUrl = "http://localhost:3000/students";
-const ul = document.querySelector(".students__list");
-const form = document.getElementById("dataForm");
-const formEdit = document.getElementById("formEdit");
-const student = document.querySelector(".student");
+const API_URL = "http://localhost:3000/students";
+
+const edit = document.querySelector("#edit");
+const form = document.querySelector("#dataForm");
+const formEdit = document.querySelector("#formEdit");
+const studentList = document.querySelector(".students__list");
 
 /**
- * Get all data from the Server
- * @param url string
+ * Build the list fecthing the data from the server url.
  */
-const getData = async (url) => {
-  let response = await fetch(url);
-  let data = await response.json();
-  return data;
-};
+function showList() {
+  fetch(API_URL).then((response) => {
+    response.json().then((data) => {
+      const listHtml = data
+        .map(
+          (student) =>
+            `<div class="student">
+              <p><span class="bold">ID:</span>${student.id}</p>
+              <p><span class="bold">Name:</span>${student.name}</p>
+              <p><span class="bold">Surname:</span>${student.surname}</p>
+              <p><span class="bold">Register:</span>${student.register}</p>
+            </div>`
+        )
+        .join("");
 
-/**
- * Print the list of element got from server
- * @param element - HTMLElement
- * @param items - Array
- */
-const printList = async (element, items) => {
-  element.innerHTML = "";
-  let data = [];
-  data = await items;
-
-  for (let i = 0; i < data.length; i++) {
-    element.innerHTML += `
-		<li>
-		<div class="student">
-			<p><span class="bold">ID: </span>${data[i].id}</p>
-			<p><span class="bold">Name: </span>${data[i].name}</p>
-			<p><span class="bold">Surname: </span>${data[i].surname}</p>
-			<p><span class="bold">Register: </span>${data[i].register}</p>
-			<button class="btn-delete" onclick="deleteElement('${data[i].id}')">Delete</button>
-			<button class="btn-edit" onclick="buildEditForm(
-				{
-					id:'${data[i].id}',
-					name: '${data[i].name}',
-					surname:'${data[i].surname}',
-					register:'${data[i].register}',
-				})">Edit</button>
-		</div>
-	</li>
-		`;
-  }
-};
-
-/**
- * Get data from the Form and send them to the server
- */
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  // Creates a new object that conintains all the data from the form
-  const prePayload = new FormData(form);
-  prePayload.append("id", uuidv4());
-  const payload = new URLSearchParams(prePayload);
-  console.log(payload);
-
-  fetch(studentUrl, {
-    method: "POST",
-    body: payload,
-  }).then((res) => {
-    // if the post has not succesfull throw and error
-    if (!res.ok) {
-      throw new Error("Failed to post");
-    }
-    // if the post is succesfull the page will be updates
-    printList(ul, getData(studentUrl));
-    return res.json();
-  });
-});
-
-formEdit.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const prePayload = new FormData(formEdit);
-  const payload = new URLSearchParams(prePayload);
-
-  const editUrl = `${studentUrl}/${payload.get("id")}`;
-
-  fetch(editUrl, {
-    method: "PUT",
-    body: payload,
-  }).then((res) => {
-    // if the post has not succesfull throw and error
-    if (!res.ok) {
-      throw new Error("Failed to put");
-    }
-    // if the post is succesfull the page will be updates
-    printList(ul, getData(studentUrl));
-    return res.json();
-  });
-});
-
-function deleteElement(id) {
-  const deleteUrl = `${studentUrl}/${id}`;
-  fetch(deleteUrl, {
-    method: "DELETE",
-  }).then((res) => {
-    // if the delete has not succesfull throw and error
-    if (!res.ok) {
-      throw new Error("Failed to delete");
-    }
-    // if the delete is succesfull remove line
+      studentList.innerHTML = listHtml;
+    });
   });
 }
 
-function buildEditForm(student) {
-  const edit = document.querySelector("#edit");
-  edit.classList.remove("hidden");
-
-  document.forms["formEdit"].name.value = student.name;
-  document.forms["formEdit"].surname.value = student.surname;
-  document.forms["formEdit"].register.value = student.register;
-  document.forms["formEdit"].id.value = student.id;
-  /*	
-	fetch(editUrl, {
-    method: "PUT",
-  }).then((res) => {
-    // if the put has not succesfull throw and error
-    if (!res.ok) {
-      throw new Error("Failed to update");
-    }
-    // if the put is succesfull show new data
-  });*/
-}
-
-/**
- * On page load all the data are retrieved
- */
-window.onload = printList(ul, getData(studentUrl));
+showList();
